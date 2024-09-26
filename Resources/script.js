70d6e73ff377d58fb6d617b92d1279d1bde0b39f
nsolax <nsola95@gmail.com>
2024-09-26 01:46:14 -0400

    nested array

--- Resources/script.js
+++ Resources/script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+let students = [
+    { name: 'Aibek', grade: 92 },
+    { name: 'Mirbek', grade: 76 },
+    { name: 'Ali', grade: 85 }
+    // Add more students here
+  ];
+
+  console.log(students[1].grade);
